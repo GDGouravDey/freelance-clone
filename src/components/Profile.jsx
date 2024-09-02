3299a4beef9e5b0c2a45d42a374e9f0dd27929ec
sudiptapaul <psudipta858@gmail.com>
2024-09-02 19:28:12 +0530

    added profile and projects page

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfileForm from './ProfileForm';
+
+const Profile = () => {
+  return (
+    <div className="">
+      <ProfileForm />
+    </div>
+  );
+};
+
+export default Profile;
