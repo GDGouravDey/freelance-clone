348c0e74f68b5eaadf6d9202fd3b3f9a68eec91f
Sayan Das <sayandasraja56@gmail.com>
2024-09-03 20:10:16 +0530

    add model for rating

---
+++ backend/src/models/review.model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose';
+
+const reviewSchema = new Schema({
+    reviewer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Employer',
+        required: true
+    },
+    reviewee: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Employee',
+        required: true
+    },
+    freelancingOffer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'FreelancingOffer',
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5
+    }
+}, { timestamps: true });
+
+export const Review = mongoose.model('Review', reviewSchema);
