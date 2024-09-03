850bfe8859efc7fb154d036d70ac094c13b49eb7
Gourav Dey <gdgouravdey@gmail.com>
2024-09-03 12:51:46 +0530

    Updated routes in main.jsx
    Reverted the changes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App.jsx";
+import Profile from "./components/Profile";
+import MyProjects from './components/MyProjects';
 import Choose from "./components/Choose.jsx";
 import LoginPage from "./components/LoginPage.jsx";
 import "./index.css";
@@ -14,6 +16,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App />} />
         <Route path="/choose" element={<Choose />} />
         <Route path="/sign-in" element={<LoginPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/myprojects" element={<MyProjects />} />
       </Routes>
     </Router>
   </React.StrictMode>
